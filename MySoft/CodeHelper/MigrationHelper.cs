24142b07fb08a5b7ad5668ace37bdc1bfa3cb3d9
“Nayeem-Bhuiyangit config --global user.email “nayeembhuiyan40@gmail.com <58185521+Nayeem-Bhuiyan@users.noreply.github.com>
2023-05-05 08:07:12 +0600

    fgdfg

--- MySoft/CodeHelper/MigrationHelper.cs
+++ MySoft/CodeHelper/MigrationHelper.cs
@@ -2,14 +2,16 @@
 {
     public class MigrationHelper
     {
+
+
+
+
+
         //Location
         //Tools > Nugate-package_manager > Nugate Package Manager Console
         //Command
         //Add-Migration MigrationName
         //Update-Database --verbose
-
-
-
 
 
 
